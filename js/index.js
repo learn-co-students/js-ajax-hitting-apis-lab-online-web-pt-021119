a812756ef9ebf1c89de273561db1c8619308d01e
cyantis <cyantis@gmail.com>
2019-09-08 15:57:39 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function getRepositories() {
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>')
+    .map(r => '<li>' + r.repository + ' - <a href="#" data-repo="' + r.repository + '" onclick="getCommits(this)">Get Commits</a></li>')
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
