e2a8b0b453bfaaae188751f43d81ef4e54fce77b
cyantis <cyantis@gmail.com>
2019-09-08 18:55:08 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 function getRepositories() {
   const username = document.getElementById("username").value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
   var repos = JSON.parse(this.responseText);
+  console.log(repos);
   const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>')
+    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a> - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' | <a href="#" data-repository="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>')
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
@@ -25,18 +26,44 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  var cleanCommits = commits.filter(function (el) {
+  const cleanCommits = commits.filter(function (el) {
   return el.author != null;});
-  console.log(cleanCommits);
   const commitsList = `<ul>${cleanCommits
     .map(
       commit =>
         '<li><strong>' +
         commit.author.login +
         '</strong> - ' +
+        '<li><strong>' +
+        commit.commit.committer.name +
+        '</strong> - ' +
         commit.commit.message +
         '</li>'
     )
     .join('')}</ul>`;
   document.getElementById("details").innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const username = document.getElementById("username").value;
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', `https://api.github.com/repos/${username}/${name}/branches`);
+  req.send();
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  //const cleanBranches = commits.filter(function (el) {return el.author != null;});
+  console.log(branches);
+  const branchesList = `<ul>${branches
+    .map(
+      branch =>
+        '<li><strong>' +
+        branch.name +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById("details").innerHTML = branchesList;
 }
